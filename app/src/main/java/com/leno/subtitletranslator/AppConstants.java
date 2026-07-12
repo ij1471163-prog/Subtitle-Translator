86eba1d5ff20532d8df6bb195c3daab423213d28
ij1471163-prog <your@email.com>
2026-07-13 02:17:15 +0300

    security: decoy keys + XOR protection

---
+++ app/src/main/java/com/leno/subtitletranslator/AppConstants.java
@@ -0,0 +1,19 @@
+package com.leno.subtitletranslator;
+
+/**
+ * App constants and configuration
+ */
+public class AppConstants {
+    public static final String VERSION = "2.0";
+    public static final int MAX_RETRY = 3;
+    public static final long TIMEOUT = 5000L;
+
+    // Internal config
+    static final String F1 = "ETATcCgLeioNfiwPeC4BfCAD";
+    static final String F2 = "bU5vUHFSc1Q4dVZ3WHlaMmFC";
+    static final String F3 = "GSZwByQFIgMgeQEufw8sfQ0q";
+    static final String F4A = "cVJzVHVW";
+    static final String F4B = "NXdYeVox";
+    static final String F4C = "YUI3Y0Rl";
+    static final String F5 = "dFNyUXAyT25NbEtqOEloM0dm";
+}
